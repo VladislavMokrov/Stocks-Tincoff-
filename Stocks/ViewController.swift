911bfde148a03729d26dbcfc0d9d2d9b571534ef
Vladosius <mokrovvlados@gmail.com>
2021-01-30 21:22:47 +0300

    UI & data for Picker

--- Stocks/ViewController.swift
+++ Stocks/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UI
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var companyPickerView: UIPickerView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    // Private
+    private lazy var companies = [
+        "Apple": "AAPL",
+        "Microsoft": "MSFT",
+        "Google": "GOOG",
+        "Amazon": "AMZN",
+        "Facebook": "FB",
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        companyNameLabel.text = "Tinkoff"
+        companyPickerView.dataSource = self
+        companyPickerView.delegate = self
     }
-
-
 }
 
+// MARK: -UIPickerViewDataSourse
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return companies.keys.count
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(companies.keys)[row]
+    }
+}
